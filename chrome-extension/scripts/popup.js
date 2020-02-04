d03e759e081b9cc18ee4954cc451e2764889c2a8
Utopian Garden <44868960+ashi-garden@users.noreply.github.com>
2020-02-04 22:49:59 +0900

    1.0.14
    Transfer 'checkDB' function to API.
    Separate 'refillMQ' function from 'Download' function and 'Favorite' function.

--- chrome-extension/scripts/popup.js
+++ chrome-extension/scripts/popup.js
@@ -1,21 +1,55 @@
+let xhr = new XMLHttpRequest();
+
 // test
-const addRequest = (addTab) => {
-  let inputKey = document.getElementById('tag');
+const addReq = async (addTab) => {
+  let addKey = document.getElementById('tag');
 
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", 'http://localhost:3000/addItem');
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({'table':addTab, 'tag':inputKey.value}));
+  fetch('http://localhost:3000/addItem', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      'table': addTab,
+      'tag': addKey.value
+    }),
+  });
 
-  inputKey.value = "";
+  addKey.value = "";
+}
+
+// test
+const chkReq = async () => {
+  let getKey = document.getElementById('tag');
+  let res;
+
+  try {
+    res = await fetch('http://localhost:3000/checkDb?tag=' + getKey.value, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+  } catch(err) {
+    alert(err);
+  }
+
+  alert((await res.json()).tag);
+  getKey.value = "";
 }
 
 // test
 document.getElementById('download').addEventListener('click', () => {
-  addRequest('Download');
+  addReq('Download');
 }, false);
 
 // test
 document.getElementById('favorite').addEventListener('click', () => {
-  addRequest('Favorite');
+  addReq('Favorite');
+}, false);
+
+// test
+document.getElementById('check').addEventListener('click', () => {
+  chkReq();
 }, false);
