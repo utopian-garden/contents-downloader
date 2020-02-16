18d79b0f4cacb51b81ffda6bcdb78d55cfa92be8
Utopian Garden <44868960+ashi-garden@users.noreply.github.com>
2020-02-16 22:57:59 +0900

    1.0.27
    Fix cannot 'Link Get' issue.

--- chrome-extension/scripts/link-getter.js
+++ chrome-extension/scripts/link-getter.js
@@ -1,7 +1,7 @@
 // リンクを取得してバックグラウンドへ送信
 chrome.runtime.onMessage.addListener(msg => {
   switch (msg.type) {
-    case 'open':
+    case 'get':
       document.querySelectorAll('[class*="fa-magnet"]').forEach(link => {
         chrome.runtime.sendMessage({
           type: "url",
