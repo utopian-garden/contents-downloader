ce86cad8be3eb9d084627a0f923a9b1fa2e8c8a3
Utopian Garden <44868960+ashi-garden@users.noreply.github.com>
2020-02-16 17:10:04 +0900

    Revert "1.0.25"
    This reverts commit 1911f47cd8461b717aef708fac60fe36de304b8e.

--- chrome-extension/scripts/link-getter.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(msg => {
-  document.querySelectorAll('[class*="fa-magnet"]').forEach(link => {
-    chrome.runtime.sendMessage({
-      type: "url",
-      value: link.parentNode.href
-    });
-  });
-});
