f314a4a08e0d4bd60e6270ef789077fcc4676516
Utopian Garden <44868960+ashi-garden@users.noreply.github.com>
2020-02-18 22:13:43 +0900

    1.0.28
    Separete 'Downloader Extension' and 'LinkGet Extension'.

--- download-extension/scripts/background.js
+++ download-extension/scripts/background.js
@@ -36,43 +36,12 @@ chrome.contextMenus.create({
 
 // コンテキストメニューの Open をクリックした場合
 chrome.contextMenus.create({
-  title: "Open",
+  title: "Link Open",
   contexts: ["page"],
   type: "normal",
   onclick: info => {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {'type':'open'});
     });
-  }
-});
-
-// コンテキストメニューの Get をクリックした場合
-chrome.contextMenus.create({
-  title: "Get",
-  contexts: ["page"],
-  type: "normal",
-  onclick: info => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {'type':'get'});
-    });
-  }
-});
-
-// コンテンツからメッセージを受信した場合
-chrome.runtime.onMessage.addListener(msg => {
-  switch (msg.type) {
-    case "url":
-      const url = msg.value;
-      fetch('http://localhost:3000/getLink', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          'url': url
-        }),
-      });
-
-      break;
   }
 });
