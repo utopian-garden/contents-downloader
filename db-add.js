0a34873210efacd699f548df6e0785a833f4c33a
Utopian Garden <44868960+ashi-garden@users.noreply.github.com>
2020-11-05 02:45:03 +0900

    Add db-add function

---
+++ db-add.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs-extra');
+const ddb = require('./lib/ddb');
+const appConfig = require('./config/app-config.json');
+
+exports.dbAdd = async (dlJson) => {
+  fs.readFile(dlJson, "utf-8", (err, data) => {
+    if (err) throw err;
+
+    for (const reqTag of JSON.parse(data)) {
+      console.log(reqTag);
+
+      const dlTable = appConfig.db.tab.dlTable;
+      const favTable = appConfig.db.tab.favTable;
+
+      try {
+        ddbUpdate(dlTable, reqTag);
+        ddbDelete(favTable, reqTag);
+      } catch(err) {
+        console.log(err.message);
+      }
+    }
+  });
+}
+
+exports.dbAdd("./config/download.json");
+
+// DB更新
+const ddbUpdate = async (reqTable, reqTag) => {
+  const updParams = {
+    TableName: reqTable,
+    Key: {
+      'tag': reqTag
+    }
+  };
+
+  try {
+    await ddb.updateItem(updParams);
+    console.log('update:' ,updParams);
+  } catch(err) {
+    console.log(JSON.stringify(err));
+    throw err;
+  }
+}
+
+// DB削除
+const ddbDelete = async (reqTable, reqTag) => {
+  const delParams = {
+    TableName: reqTable,
+    Key: {
+      'tag': reqTag
+    }
+  };
+
+  try {
+    await ddb.deleteItem(delParams);
+    console.log('delete:', delParams);
+  } catch(err) {
+    console.log(JSON.stringify(err));
+    throw err;
+  }
+}
