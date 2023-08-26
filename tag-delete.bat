c6acb54ef1c484ecf992830a67e65070c65cd020
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2023-08-26 19:29:40 +0900

    Update

--- tag-delete.bat
+++ tag-delete.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Delete
-node ./lib/tag.js Delete "same_(kodomo_no_koe)" "love_live!_(series) same -same_(kodomo_no_koe) order:quality -rating:safe" "off"
+node ./lib/tag.js Del "school_unform" "school_unform" "off"
 
 pause
