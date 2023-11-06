bae9214316811db0322cde7e7fb205d99876052b
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2023-11-06 18:13:52 +0900

    Update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "school_uniform" "school_unform" "off"
+node ./lib/tag.js Add "yamaishi_(mukoubuti)" "yamaishi -yamaishi_(mukoubuti) -rating:safe" "off"
 
 pause
