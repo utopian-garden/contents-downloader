3bd9a5bce9cc6e63135527103e8928d100c52ab9
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2024-10-21 02:48:33 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "maid" "user:ashi_garden yamakaze_(kantai_collection) -maid" "off"
+node ./lib/tag.js Add "penis" "user:ashi_garden tekoki -penis" "off"
 
 pause
