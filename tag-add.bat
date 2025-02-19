de9c2ccef4369d4e6b8efe1e81d99b867feab2a5
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-02-20 00:11:06 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "prolapse" "uterine_prolapse -prolapse" "off"
+node ./lib/tag.js Add "marvel_comics" "hydrafxx spider-man_(series) -marvel_comics" "off"
 
 pause
