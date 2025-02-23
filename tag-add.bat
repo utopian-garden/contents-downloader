d2aa517f36161c790cebb5ca9f6a216147933c18
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-02-23 23:17:58 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "marvel_comics" "hydrafxx spider-man_(series) -marvel_comics" "off"
+node ./lib/tag.js Add "metroid" "laosduude samus_aran -metroid" "off"
 
 pause
