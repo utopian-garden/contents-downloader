61df1ad26957c473bbdd36fbbd4895eac1e01df8
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-02-25 23:10:09 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "marvel_comics" "akuji_saito big_hero_6 -marvel_comics" "off"
+node ./lib/tag.js Add "fatal_fury" "bouquetman shiranui_mai -fatal_fury" "off"
 
 pause
