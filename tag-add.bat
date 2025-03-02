a88280b38b24e885331a7beb38796dc554c3c67d
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-03-03 00:40:45 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "fanza_games" "detariki_z -fanza_games" "off"
+node ./lib/tag.js Add "surume_(ishummd)" "surume3dcg -surume_(ishummd)" "off"
 
 pause
