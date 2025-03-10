2cd808992bb7ce13de5dce4ea7fdb6fc0f2e9a75
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-03-10 11:41:02 +0900

    update

--- tag-delete.bat
+++ tag-delete.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Delete
-node ./lib/tag.js Del "bunny_years" "user:ashi_garden bunny_years" "off"
+node ./lib/tag.js Del "original" "user:ashi_garden azur_lane original" "off"
 
 pause
