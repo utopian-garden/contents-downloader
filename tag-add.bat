5f8485dd2a1ea37c9dfeed5c04c0e37bbe3f0d3a
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-03-10 11:41:02 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "blue_nights" "user:ashi_garden ‚Ô‚é[‚È‚¢‚Â -blue_nights" "off"
+node ./lib/tag.js Add "koizakura_maikaze_(quappa-el)" "user:ashi_garden original maloxx -koizakura_maikaze_(quappa-el)" "off"
 
 pause
