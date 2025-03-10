1dffae1d48c96a9fe883e3d7f067bdb0271b8e63
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-03-11 04:17:07 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "koizakura_maikaze_(quappa-el)" "user:ashi_garden original maloxx -koizakura_maikaze_(quappa-el)" "off"
+node ./lib/tag.js Add "spats" "user:ashi_garden housaki_sumire_(utifutubo) -spats" "off"
 
 pause
