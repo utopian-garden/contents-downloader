9766689a4350e0a2d923872d87603c39f62541e5
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-03-15 01:27:56 +0900

    update

--- tag-add.bat
+++ tag-add.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Add
-node ./lib/tag.js Add "spats" "user:ashi_garden housaki_sumire_(utifutubo) -spats" "off"
+node ./lib/tag.js Add "voiceroid" "user:ashi_garden fukkireta -voiceroid" "off"
 
 pause
