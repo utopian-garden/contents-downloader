c52b8b112fe1a90971181620656a3e2bd2899214
Utopian Garden <44868960+utopian-garden@users.noreply.github.com>
2025-03-21 06:21:20 +0900

    update

--- tag-delete.bat
+++ tag-delete.bat
@@ -1,6 +1,6 @@
 @echo off
 
 @title Tag Delete
-node ./lib/tag.js Del "potez_(mmd)" "potez_(mmd) potez_(quappa-el)" "off"
+node ./lib/tag.js Del "penetartion" "penetartion penetration" "off"
 
 pause
